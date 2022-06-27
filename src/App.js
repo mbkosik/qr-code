3e977a0fd41e597ce1dc40aee1f1272d63f6d0b7
mbkosik <72941144+mbkosik@users.noreply.github.com>
2022-06-27 19:28:04 +0200

    add qr scanner

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
 
 function App() {
+  const [data, setData] = useState('No result');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        videoStyle={{ width: '400px', height: '300px' }}
+        videoContainerStyle={{position: 'absolute', overflow: 'unset', padding: 'unset'}}
+      />
+      <p style={{position: 'absolute', top: '50%', left: '0'}}>{data}</p>
+    </>
   );
 }
 
